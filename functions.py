43e945a4e9ae06252d1d3ebb45a733e8b5eff17c
shayne2000 <st3334roongaroon.ac.th@gmail.com>
2025-08-15 17:57:47 +0700

    some kind of lineer regression with auto adjust weight

---
+++ functions.py
@@ -0,0 +1,35 @@
+import math
+import pandas as pd
+
+
+def ReLU (x) :    # clear negative values make it 0
+    return max(0,x)
+
+
+
+def sigmoid (x) : #prediction whether it's near 1 or 0       ~-inf , ~inf --> ~0 , ~1
+    return 1/(1+math.e**-x)
+
+
+def train_test_split (df,test) :
+    shuffled_df = df.sample(frac=1)
+    n = len(df)
+    test = round(test*n)
+    
+    train = n-test
+    
+    
+    return {'train':shuffled_df[0:train],'test':shuffled_df[train:n]}
+
+def soft_max (array) :
+    new_array = []
+    for i in array :
+        new_array.append(math.e**i)
+    sum_array = sum(new_array)
+    
+    ans = []
+    for i in new_array :
+        ans.append(round(i/sum_array,3))
+    
+    return ans
+    
---
+++ test.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from functions import *
+import numpy as np
+import random
+
+
+df = pd.read_csv(r"iris\versions\2\Iris.csv")
+df.dropna(inplace=True)
+
+# print(df.dropna(inplace=True))
+
+# result = train_test_split(df,0.3)
+
+# train = result['train']
+# # # print(len(train))
+
+
+# test = result['test']
+# print(len(test))
+
+dimention = len(df['Species'].unique())
+# print(dimention)
+
+# Id = df['Id']
+xs_const = df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = np.array(df['Species'])
+
+# for i in df.columns :
+#     print(df[i].unique())
+
+encode = {}
+arr = []
+for i in range(dimention) :
+    arr.append(0)
+    
+# print(arr)
+
+for i in range(dimention) :  #not sure it has to be array though
+    my_arr = arr[:]
+    my_arr[i] = 1
+    encode[df['Species'].unique()[i]] = np.array(my_arr)
+    
+    # print(df['Species'].unique()[i])
+
+for i in range(len(y)) :
+    y[i] = encode[y[i]]
+
+# print(y)
+
+# statement = y==df['Species'].unique()[i]
+# y[statement] = my_arr
+
+# print(y[y=='Iris-setosa'])
+
+# print(encode['Iris-virginica'])
+
+
+xs = xs_const.copy()
+
+# print('old :',xs[0:1].values)
+
+n_attribute = len(xs[0:1].values[0])
+
+w_sum = [0,0,0,0]
+
+step = 10
+
+
+    
+
+# print('w :',w)
+
+# print('old :',xs_const[0:1])
+# print('new :',xs[0:1].values[0]*w)
+# print('diff :',xs[0:1].values[0]*w-xs_const[0:1])
+
+
+
+def guess_weight (adjust_times,index) :
+    w = []
+    for i in range(n_attribute) :   #random ครั้งแรก  ทำครั้งเดียว
+        w.append(random.randint(-10,10))
+    for _ in range(adjust_times) :
+        for i in range(len(w)) :   #ปรับเป็น step
+            diff = xs[index:index+1].values[0][i]*w[i]-xs_const[index:index+1].values[0][i] 
+            
+            if diff != 0 :
+                w[i] -= (abs(diff)/diff)*step
+    return w
+
+for i in range(150) :
+    new_w = guess_weight(10,i)
+    # print(new_w)
+    
+        
+    for j in range(n_attribute) :
+        w_sum[j] += new_w[j]
+    
+print(np.array(w_sum)/150)
+
+
+
+# print('old :',xs_const[0:1])
+# print('new :',xs[0:1].values[0]*w)
+# print('diff :',xs[0:1].values[0]*w-xs_const[0:1])
